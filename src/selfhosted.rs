afa667bf0e95eb94614d770fa6082ed99e32128a
Dustin Martin <dustin@dmartin.io>
2024-01-03 13:51:49 -0500

    Update dependencies
    Including hyper 1.0 ecosystem crates

--- src/selfhosted.rs
+++ src/selfhosted.rs
@@ -1,14 +1,17 @@
 use crate::{Backend, BackendCreationError, BuildEvent};
 use async_trait::async_trait;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_staticfile::{Body, Static};
+use hyper_util::rt::TokioIo;
 use log::{debug, info};
 use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::Debug;
 use std::net::SocketAddr;
-use std::path::{Path, PathBuf};
+use std::path::Path;
+use tokio::net::TcpListener;
 use tokio::sync::mpsc::Receiver;
 
 #[derive(Debug)]
@@ -16,43 +19,41 @@ pub struct Selfhosted {
     address: String,
 }
 
-impl Selfhosted {
-    async fn handle_request<B>(
-        req: Request<B>,
-        cache_dir: PathBuf,
-    ) -> Result<Response<Body>, std::io::Error> {
-        let res = if req.uri().path() == "/health" {
-            http::Response::builder()
-                .status(http::StatusCode::OK)
-                .body(hyper::Body::empty())
-                .expect("Unable to build response")
-        } else if req.uri().path().ends_with(".__checksum") {
-            http::Response::builder()
-                .status(http::StatusCode::NOT_FOUND)
-                .body(hyper::Body::empty())
-                .expect("Unable to build response")
-        } else {
-            let result = hyper_staticfile::resolve(&cache_dir, &req).await?;
-            let mut response = hyper_staticfile::ResponseBuilder::new()
-                .request(&req)
-                .build(result)
-                .unwrap();
-            if response.status() == http::StatusCode::OK {
-                let headers = response.headers_mut();
-                headers.insert(
-                    http::header::CONTENT_DISPOSITION,
-                    http::HeaderValue::try_from("attachment").unwrap(),
-                );
-            }
-            response
-        };
-        info!(
-            "Serving request: {} ({})",
-            req.uri().to_string(),
-            res.status()
-        );
-        Ok(res)
-    }
+async fn handle_request<B>(
+    req: Request<B>,
+    static_: Static,
+) -> Result<Response<Body>, std::io::Error> {
+    let res = if req.uri().path() == "/health" {
+        http::Response::builder()
+            .status(http::StatusCode::OK)
+            .body(Body::Empty)
+            .expect("Unable to build response")
+    } else if req.uri().path().ends_with(".__checksum") {
+        http::Response::builder()
+            .status(http::StatusCode::NOT_FOUND)
+            .body(Body::Empty)
+            .expect("Unable to build response")
+    } else {
+        let result = static_.resolver.resolve_request(&req).await?;
+        let mut response = hyper_staticfile::ResponseBuilder::new()
+            .request(&req)
+            .build(result)
+            .unwrap();
+        if response.status() == http::StatusCode::OK {
+            let headers = response.headers_mut();
+            headers.insert(
+                http::header::CONTENT_DISPOSITION,
+                http::HeaderValue::try_from("attachment").unwrap(),
+            );
+        }
+        response
+    };
+    info!(
+        "Serving request: {} ({})",
+        req.uri().to_string(),
+        res.status()
+    );
+    Ok(res)
 }
 
 #[async_trait]
@@ -81,20 +82,25 @@ impl Backend for Selfhosted {
         cache_dir: &Path,
         mut _rx: Receiver<BuildEvent>,
     ) -> Result<(), Box<dyn Error>> {
+        let static_ = Static::new(cache_dir);
+
         let addr: SocketAddr = self.address.parse()?;
-
-        let make_service = make_service_fn(move |_| {
-            let path = PathBuf::from(cache_dir);
-            async {
-                Ok::<_, hyper::Error>(service_fn(move |req| {
-                    Selfhosted::handle_request(req, path.clone())
-                }))
-            }
-        });
-
-        let server = Server::bind(&addr).serve(make_service);
+        let listener = TcpListener::bind(addr).await?;
         info!("Starting server ({}). Press CTRL-C to exit.", &self.address);
-        server.await?;
-        Ok(())
+        loop {
+            let (stream, _) = listener.accept().await?;
+            let static_ = static_.clone();
+            tokio::spawn(async move {
+                if let Err(err) = hyper::server::conn::http1::Builder::new()
+                    .serve_connection(
+                        TokioIo::new(stream),
+                        service_fn(move |req| handle_request(req, static_.clone())),
+                    )
+                    .await
+                {
+                    eprintln!("Error serving connection: {:?}", err);
+                }
+            });
+        }
     }
 }
